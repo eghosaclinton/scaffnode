d1601d3f958ece3cfb7a27cc0ca5ddf54036956a
clintt-09 <aisosaclinton09@gmail.com>
2025-09-14 22:20:32 +0100

    initial commit

---
+++ bin/index.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import chalk from 'chalk';
+
+console.log(chalk.green('✔ Project created successfully'));
+console.log(chalk.yellow('⚠ Warning: missing .env file'));
+console.log(chalk.blue.bold('My CLI Tool'));
